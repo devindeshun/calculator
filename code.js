1ff44a71d75388cab9c3bc03b2c3de7a80fb8742
Devin <devin.dshun@gmail.com>
2025-12-30 20:56:57 -0600

    Add functions for the operations of the calculator

---
+++ code.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    
+}
+
+// console.log([3,5,6,'+',4,'/',2,7].find(operator => operator == "+" || operator == "-" || operator == "*" || operator == "/"))
+
+let var1;
+let var2;
+let var3;
