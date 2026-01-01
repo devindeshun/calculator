16f736b50c842c69b99977930b830d6785d8fc6c
Devin <devin.dshun@gmail.com>
2026-01-01 16:20:18 -0600

    Add operator area and update logic to clear it

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
+
+
+let numpadNumberKeys = [...document.querySelectorAll('.numpad-number')];
+let numpadSymbolKeys = [...document.querySelectorAll('.numpad-symbol')];
+let textArea = document.querySelector('.calculator-input');
+let operatorArea = document.querySelector('.calculator-operator');
+let clearKey = document.querySelector('.numpad-clear');
+let backspaceKey = document.querySelector('.numpad-backspace');
+let arr = [];
+let operator = "";
+let num1 = "";
+let num2 = "";
+let screen = "";
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -18,32 +32,30 @@ function operate(operator, num1, num2) {
     
 }
 
-let numpadNumberKeys = [...document.querySelectorAll('.numpad-number')];
-let textArea = document.querySelector('.calculator-input');
-let clearKey = document.querySelector('.numpad-clear');
-let backspaceKey = document.querySelector('.numpad-backspace');
-let screen = "";
-let arr = [];
-
-clearKey.addEventListener('click', () => {
-    screen = "";
-    updateScreen(screen);
-});
-
-backspaceKey.addEventListener('click', () => {
-    arr = screen.split('').slice(0,-1).join('')
-    screen = '';
-    updateScreen(arr);
-});
-
 function updateScreen(newValue) {
     screen = screen + newValue;
     textArea.value = screen;
 }
 
+clearKey.addEventListener('click', () => {
+    screen = "";
+    operatorArea.value = "";
+    updateScreen(screen);
+});
+
+backspaceKey.addEventListener('click', () => {
+    arr = screen.split('').slice(0,-1).join('')
+    screen = "";
+    updateScreen(arr);
+});
+
 numpadNumberKeys.map((item) => item.addEventListener('click', (e) => {
     updateScreen(e.target.innerHTML);
 }));
+
+numpadSymbolKeys.map((item) => item.addEventListener('click', (e) => {
+    operatorArea.value = e.target.innerHTML;
+}))
 
 // console.log([3,5,6,'+',4,'/',2,7].find(operator => operator == "+" || operator == "-" || operator == "*" || operator == "/"))
 
