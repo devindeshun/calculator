ef5171adf473bcfdb272fa2d71cdd7bbbd3abe9c
Devin <devin.dshun@gmail.com>
2026-01-07 16:44:42 -0600

    Add keyboard functionality

--- code.js
+++ code.js
@@ -65,6 +65,66 @@ function clearScreen() {
     screen = "";
 }
 
+document.addEventListener('keyup', (e) => {
+    numKeys = "0 1 2 3 4 5 6 7 8 9";
+    symKeys = "+ - * / Enter =";
+    decKey = ".";
+    bkspKey = "Backspace";
+    clrKey = "Escape";
+    numKeys = numKeys.split(' ');
+    symKeys = symKeys.split(' ');
+    
+    if (numKeys.includes(e.key)) {
+        updateScreen(e.key);
+        operatorArea.value = '';
+    } else if (symKeys.includes(e.key)) {
+        let keyedSymbol = e.key;
+        if (keyedSymbol == "Enter") {
+            keyedSymbol = "=";
+        }
+
+        if (keyedSymbol != '=') {
+            operatorArea.value = keyedSymbol;
+            operator = keyedSymbol;
+        }
+
+        if (!num1) {
+            num1 = screen;
+        } else {
+            num2 = screen;
+        }
+
+        if (num1 && num2) {
+            num1 = operate(operator, num1, num2);
+            num2 = "";
+        }
+
+        clearScreen();
+
+        if (keyedSymbol == '=') {
+            ["keyup", "mousedown"].forEach( (event) => {
+                document.body.addEventListener(event, (e) => {
+                    const numpadPressed = e.target.className.split(' ').some((e) => e == 'numpad-number') || numKeys.includes(e.key);
+                    const backspacePressed = e.target.className.split(' ').some((e) => e == 'numpad-backspace') || e.key == bkspKey;
+                    if (numpadPressed || backspacePressed) {
+                        clearAll();
+                    };
+                }, {once : true})
+        })}
+    } else if (e.key == decKey) {
+        if (!screen.includes('.')){
+            updateScreen(e.key);
+        }
+    } else if (e.key == bkspKey) {
+        backspacedNumber = screen.split('').slice(0,-1).join('');
+        clearScreen();
+        updateScreen(backspacedNumber);
+    } else if (e.key == "Escape") {
+        clearAll();
+        updateScreen(screen);
+    }
+});
+
 clearKey.addEventListener('click', () => {
     clearAll();
     updateScreen(screen);
@@ -85,9 +145,6 @@ decimalKey.addEventListener('click', (e) => {
 numpadNumberKeys.map((item) => item.addEventListener('click', (e) => {
     updateScreen(e.target.innerHTML);
     operatorArea.value = "";
-    if (!num2 && num1) {
-        num2 = screen;
-    }
 }));
 
 numpadSymbolKeys.map((item) => item.addEventListener('click', (e) => {
@@ -97,14 +154,15 @@ numpadSymbolKeys.map((item) => item.addEventListener('click', (e) => {
         operator = clickedSymbol;
     }
 
-    if (num1 && num2) {
-        num2 = screen;
-        num1 = operate(operator, num1, num2);
-        num2 = "";
-    }
-
     if (!num1) {
         num1 = screen;
+    } else {
+        num2 = screen;
+    }
+
+    if (num1 && num2) {
+        num1 = operate(operator, num1, num2);
+        num2 = "";
     }
 
     clearScreen();
